292d5739d2062819331da0c024a66f9ee9f38c4e
sanjeet kumar <skc0845@gmail.com>
2024-02-23 15:30:56 +0530

    added product add page

--- frontend/src/pages/Addproduct.tsx
+++ frontend/src/pages/Addproduct.tsx
@@ -1,5 +1,81 @@
-const Addproduct = () => {
-  return <div>Addproduct</div>;
+import { FC } from "react";
+import logo from "../assets/logo.png";
+
+const Addproduct: FC = () => {
+  return (
+    <section className="py-3 h-screen w-screen bg-violet-100">
+      <div className="w-full  mb-12  px-4 mx-auto mt-1">
+        <img
+          src={logo}
+          alt="logo_website"
+          className="w-40 mx-auto self-center mb-3"
+        />
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+          <div className="rounded-t mb-0 px-4 py-3 border-0">
+            <div className="flex flex-wrap items-center">
+              <div className="relative w-screen px-4 max-w-screen flex-grow flex-1">
+                <h3 className="font-bold text-2xl text-blueGray-700">
+                  Products
+                </h3>
+              </div>
+              <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                <button
+                  className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                >
+                  + Add Item
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="block w-full overflow-x-auto">
+            <table className="items-center bg-transparent w-full border-collapse ">
+              <thead>
+                <tr>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Product Name
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Rate
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Quantity
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Total
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    GST
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                    Book
+                  </th>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                    500
+                  </td>
+                  <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    2
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    1000
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {180}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Addproduct;
